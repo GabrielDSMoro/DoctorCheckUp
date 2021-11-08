57b05ef4ebdd12dc101708e8da750f9152534235
Gabriel <gabrieldsmoro@gmail.com>
2021-11-08 15:46:26 -0300

    "Criação de feature de simulação de testes dos sensores nas máquinas"

--- barramento-de-eventos/index.js
+++ barramento-de-eventos/index.js
@@ -12,6 +12,8 @@ app.post('/eventos', (req, res) => {
     axios.post('http://localhost:4000/eventos', evento)
     // envia o evento para o microsserviço de tanques
     axios.post('http://localhost:2000/eventos', evento)
+    // envia o evento para o microsserviço de testes
+    axios.post('http://localhost:5000/eventos', evento)
     //envia o evento para o microsserviço de consulta
     axios.post('http://localhost:6000/eventos', evento)
     // envia o evento para o microsserviço de classificação
